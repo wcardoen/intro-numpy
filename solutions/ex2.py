0674ad05880f2834aba93de114c1bf20072c634d
wrc <sleipnir@x1.lan>
2023-10-23 12:01:22 -0600

    10/23/2023

--- solutions/ex2.py
+++ solutions/ex2.py
@@ -1,6 +1,4 @@
 import numpy as np
-import numpy.linalg as la
-np.set_printoptions(precision=4)
 
 print(f"Exercise 2::")
 print(f"  2.1:")
@@ -8,17 +6,14 @@ a  = np.fromfunction(lambda x,y: x+y,(5,5),dtype='int32')
 print(f" a:\n{a}\n")
 
 print(f"  2.2:")
+# Solution 1:
 b = np.eye(6,6) + np.eye(6,6,k=1) + np.eye(6,6,k=-1)
 b = b.astype(dtype='bool')
 print(f" b:\n{b}\n")
 
-print(f"  2.3:")
-p = 0.7
-n = 10000
-sz= 8
-up  = np.diag([1.0] + (sz-2)*[p], k= 1)
-low = np.diag((sz-2)*[1.0-p] + [1.0], k=-1)
-P = up+low
-print(f" P:\n{P}")
-Pn = la.matrix_power(P,n)
-print(f" P^{n}:\n{Pn}")
+# Solution 2:
+c = np.diag([1 for i in range(6)]) +\
+    np.diag([1 for i in range(5)], k=1) +\
+    np.diag([1 for i in range(5)],k=-1)
+c = c.astype(dtype='bool')
+print(f" c:\n{c}\n")    
